4f6a037fb811c90d336430b37f47ac173e86d4c6
miguepollo <miguelgarciapel@protonmail.com>
2023-06-23 09:26:55 +0200

    Conseguir que lo implima ur codigo QR por el serial.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,49 @@ void generar_direcciones_derivadas_de_xpub(){
   }   
 }
 
+
+void codigoqrprueba() {
+ Serial.begin(115200);
+
+    // Start time
+    uint32_t dt = millis();
+  
+    // Create the QR code
+    QRCode qrcode;
+    uint8_t qrcodeData[qrcode_getBufferSize(3)];
+    qrcode_initText(&qrcode, qrcodeData, 3, 0, "SUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+  
+    // Delta time
+    dt = millis() - dt;
+    Serial.print("QR Code Generation Time: ");
+    Serial.print(dt);
+    Serial.print("\n");
+
+    // Top quiet zone
+    Serial.print("\n\n\n\n");
+
+    for (uint8_t y = 0; y < qrcode.size; y++) {
+
+        // Left quiet zone
+        Serial.print("        ");
+
+        // Each horizontal module
+        for (uint8_t x = 0; x < qrcode.size; x++) {
+
+            // Print each module (UTF-8 \u2588 is a solid block)
+            Serial.print(qrcode_getModule(&qrcode, x, y) ? "\u2588\u2588": "  ");
+
+        }
+
+        Serial.print("\n");
+    }
+
+    // Bottom quiet zone
+    Serial.print("\n\n\n\n");
+}
+
 void loop() {
-  generar_direcciones_derivadas_de_xpub();
-  delay(3000);
+generar_direcciones_derivadas_de_xpub();
+delay(7000);
+codigoqrprueba();
 }
